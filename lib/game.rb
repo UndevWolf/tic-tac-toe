4013c6524f433a8a47773a25e254bbb9322ef9eb
IslesPJ <isles.pj@gmail.com>
2026-06-05 10:12:55 +0900

    Optimized code to a more OOP design.

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,10 @@
 class GameRound
   attr_accessor :cell
 
-  def initialize
+  def initialize(player_one, player_two)
     @cell = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @player_one = player_one
+    @player_two = player_two
     display_grid
   end
 
@@ -22,6 +24,37 @@ class GameRound
   def filled?
     (1..9).all? do |i|
       cell[i - 1] != ' '
+    end
+  end
+
+  def play
+    [@player_one, @player_two].cycle do |player|
+      choice = player.play
+      if player == @player_one && cell[choice - 1] == ' '
+        cell[choice - 1] = 'O'
+        player.owned_cells << choice
+      elsif player == @player_two && cell[choice - 1] == ' '
+        cell[choice - 1] = 'X'
+        player.owned_cells << choice
+      elsif cell[choice - 1] != ' '
+        puts 'Box is already picked.'
+        redo
+      end
+      display_grid
+      if [1, 5, 9].all? { |i| player.owned_cells.include?(i) } ||
+         [3, 5, 7].all? { |i| player.owned_cells.include?(i) } ||
+         [1, 4, 7].all? { |i| player.owned_cells.include?(i) } ||
+         [2, 5, 8].all? { |i| player.owned_cells.include?(i) } ||
+         [3, 6, 9].all? { |i| player.owned_cells.include?(i) } ||
+         [1, 2, 3].all? { |i| player.owned_cells.include?(i) } ||
+         [4, 5, 6].all? { |i| player.owned_cells.include?(i) } ||
+         [7, 8, 9].all? { |i| player.owned_cells.include?(i) }
+        puts "#{player.name} wins!"
+        break
+      elsif filled?
+        puts "It's a tie."
+        break
+      end
     end
   end
 end
