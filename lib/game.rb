ef59b0c0441f636ab8f8155ce433a222d119fbdb
IslesPJ <isles.pj@gmail.com>
2026-06-05 11:32:54 +0900

    Appeased our lord rubocop.

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,9 @@
 
 # tic tac toe main game class.
 class GameRound
-  attr_accessor :cell
+  attr_accessor :cell, :player_one, :player_two
 
+  WIN = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
   def initialize(player_one, player_two)
     @cell = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     @player_one = player_one
@@ -13,7 +14,7 @@ class GameRound
 
   def display_grid
     clear
-    puts "
+    puts "TIC TAC TOE
     #{@cell[0]}   |   #{@cell[1]}   |  #{@cell[2]}
   ------|-------|------
     #{@cell[3]}   |   #{@cell[4]}   |  #{@cell[5]}
@@ -28,33 +29,41 @@ class GameRound
   end
 
   def play
-    [@player_one, @player_two].cycle do |player|
+    [player_one, player_two].cycle do |player|
       choice = player.play
-      if player == @player_one && cell[choice - 1] == ' '
-        cell[choice - 1] = 'O'
-        player.owned_cells << choice
-      elsif player == @player_two && cell[choice - 1] == ' '
-        cell[choice - 1] = 'X'
-        player.owned_cells << choice
-      elsif cell[choice - 1] != ' '
-        puts 'Box is already picked.'
+      if choice < 1 || choice > 9 || cell[choice - 1] != ' '
+        puts 'invalid'
         redo
       end
+      player_turn(choice, player)
       display_grid
-      if [1, 5, 9].all? { |i| player.owned_cells.include?(i) } ||
-         [3, 5, 7].all? { |i| player.owned_cells.include?(i) } ||
-         [1, 4, 7].all? { |i| player.owned_cells.include?(i) } ||
-         [2, 5, 8].all? { |i| player.owned_cells.include?(i) } ||
-         [3, 6, 9].all? { |i| player.owned_cells.include?(i) } ||
-         [1, 2, 3].all? { |i| player.owned_cells.include?(i) } ||
-         [4, 5, 6].all? { |i| player.owned_cells.include?(i) } ||
-         [7, 8, 9].all? { |i| player.owned_cells.include?(i) }
-        puts "#{player.name} wins!"
-        break
-      elsif filled?
-        puts "It's a tie."
-        break
-      end
+      check_result(player)
     end
+  end
+
+  def player_turn(choice, player)
+    if player == player_one
+      cell[choice - 1] = 'O'
+      player.owned_cells << choice
+    elsif player == player_two
+      cell[choice - 1] = 'X'
+      player.owned_cells << choice
+    end
+  end
+
+  def check_result(player)
+    if WIN.any? do |i|
+      i.all? { |i| player.owned_cells.include?(i) }
+    end
+      puts "#{player.name} wins!"
+      exit
+    elsif filled?
+      puts "It's a tie."
+      exit
+    end
+  end
+
+  def clear
+    Gem.win_platform? ? system('cls') : system('clear')
   end
 end
--- main.rb
+++ main.rb
@@ -3,13 +3,8 @@
 require './lib/player'
 require './lib/game'
 
-def clear
-  Gem.win_platform? ? system('cls') : system('clear')
-end
-
 puts "Enter Player 1's name: "
 player_one = Player.new(gets.chomp)
 puts "Enter Player 2's name: "
 player_two = Player.new(gets.chomp)
-clear
 GameRound.new(player_one, player_two).play
